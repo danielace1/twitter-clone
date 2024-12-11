973a33b18d2928254a0a065fa4eb7d2db1be564d
danielace1 <danielsudharsan1805@gmail.com>
2024-12-12 01:06:35 +0530

    fix: server.js

--- server/server.js
+++ server/server.js
@@ -29,6 +29,7 @@ app.use(
     credentials: true,
   })
 );
+
 app.use(
   express.json({
     limit: "5mb",
@@ -36,6 +37,7 @@ app.use(
 );
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.options("*", cors());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
